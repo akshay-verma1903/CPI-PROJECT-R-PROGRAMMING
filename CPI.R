aa8f1d5f498cb7ef35357899a2b72f78070d9761
Akshay Verma <52961641+akshay-verma1903@users.noreply.github.com>
2021-10-28 15:52:56 +0530

    Update CPI.R

--- CPI.R
+++ CPI.R
@@ -1,5 +1,5 @@
 
-## Loading all the Libraries ##
+### Loading all the Libraries ###
 
 library(ggplot2)
 library(tidyverse)
@@ -7,17 +7,13 @@ library(tseries)
 library(dplyr)
 library(psych)
 
-### Read the CPI XLSX file ###
+setwd("C:/Users/aksha/Videos") # Set the path where file located
 
-getwd()
-#list.files()
-setwd("C:/Users/aksha/Videos/1st Semester/PGDDA-117 Project-1/PGDDA - 117 Project")
-
-cpi_data <- read.xlsx("CPI.xlsx",1) # Read the sheet named 'MADHYA_PRADESH'
+cpi_data <- read.xlsx("CPI.xlsx",1)
 
 ### Exploratory Data Analysis ###
 
-dim(cpi_data)
+dim(cpi_data) # To show the dimensions of data
 
 names(cpi_data) # To list the column names 
 
@@ -33,7 +29,7 @@ boxplot(as.data.frame(cpi_data)[3:12]) # To show the Outliers
 
 is.na(as.data.frame(cpi_data)[1:10,3:12]) # To check that is there is any missing value or not.
 
-### Do it for each commodity seperately ###
+### Exploratory Data Analysis End ###
 
 # Converting data into time series object
 
@@ -46,33 +42,9 @@ autoplot(cpi_ts)+
   xlab("Year")+
   ylab("CPI Index")
 
-# Decomposition of time series plot sing STL to detect anomaly detection
-
-cpi_decomposition=stl(cpi_ts, s.window = "p")
-
-autoplot(cpi_decomposition, main = "Decomposition by STL method")
-
-#Observations:
-# 1. Seasonality keeps changing with time. Fluctuation are clearly vsisible and increase with time.
-# 2. Increasing trend in series
-# 3. There are some random variations.
-# 4. There are 3 components - Trend , Seasonality and Residual (Random, Noise, Irregular, Remainder) error 
-
-#Decompose Time series data into additive components
-
-#cpi_tS_add=decompose(cpi_ts, type="additive")
-
-#autoplot(cpi_tS_add, main = "Decomposition by Decompse additive method")
-
-#Decompose Time series data into multiplicative components
-
-#cpi_ts_multi=decompose(cpi_ts, type="multiplicative")
-
-#autoplot(cpi_ts_multi, main = "Decomposition by Decompse multiplicative method")
-
 ### Methods to find stationarity of a data set ###
 
-# 1. Checking stationarity by using Auto Correlation Function
+## 1. Checking stationarity by using Auto Correlation Function ##
 
 lags=window(cpi_ts, start = c(2013,1))
 
@@ -80,41 +52,30 @@ ggAcf(lags) # ACF of Original time series shows downwards trend which says that 
 
 ## 2. Kwiatkowski-Phillips-Schmidt-Shin (KPSS) ##
 
-kpss.test(cpi_ts) #Its is non-stationary because the values is less than 0.05
+kpss.test(cpi_ts) # Its is non-stationary because the values is less than 0.05
 
 ## 3. Phillips-Perron test (PP) Test ##
 
-## pp.test(cpi_ts) #Its is non-stationary because the values is greater than 0.05
+pp.test(cpi_ts) #Its is non-stationary because the values is greater than 0.05
 
 #################################
 
-
 ### Converting Non Stationary to Stationary ###
 
-# cpi_diff = diff(cpi_ts) # Taking difference
+cpi_diff = diff(cpi_ts) # Taking difference
 
-# autoplot(cpi_diff, main="First Order Differencing")
+autoplot(cpi_diff, main="First Order Differencing")
 
 ### TRAINING DATA AND TEST DATA ###
 
 cpi_train= window(cpi_ts, end=c(2020,03), frequency=12) # Training Data Set
 cpi_test= window(cpi_ts, start=c(2020,04),frequency=12) # Test Data Set
 
-# check autocorrelation function
-
-# acf(cpi_train)
-
-# Check Partial Correlational function
-
-# pacf(cpi_train)
-
 ### MODEL 1 - ARIMA MODEL ####
 
 # Auto ARIMA Model
 
 cpi_arima<-auto.arima(cpi_train, seasonal = TRUE, ic="bic") # Creating an arima object
-
-#summary(cpi_arima) # Summary of the arima object
 
 # Forecasting
 
@@ -140,14 +101,6 @@ colnames(cpi_table_arima) <- c("Actual Value", "Forecasted Value", "Error") # Re
 
 view(cpi_table_arima)
 
-# Accuracy of the forecast with the actual values
-
-#accuracy(cpi_arima_forecast, cpi_test)
-
-# Check residuals and histograms
-
-#checkresiduals(cpi_arima_forecast) 
-
 #### ARIMA MODEL END ###
 
 ### MODEL 2 - Holt-Winter's additive method (Triple exponential smoothing) ###
@@ -158,10 +111,6 @@ autoplot(cpi_hw) +
   ggtitle("India | Forecast CPI for Cereals and products using Holt-Winter's additive method")+
   xlab("Year")+
   ylab("CPI Index")
-
-#cpi_hw$model # Defination of the model
-
-#accuracy(cpi_hw, cpi_test) # Accuracy of the forecast with the actual values
 
 ## Actual vs Forecast in Holt's Winter Model ##
 
@@ -180,12 +129,9 @@ view(cpi_table_hw)
 
 ### Holt-Winter's additive method (Triple exponential smoothing) ends ###
 
-
 ### Model 3 - Neural Network Model ###
 
 cpi_neural=nnetar(cpi_train)
-
-# summary(cpi_neural) # Summary of the model
 
 cpi_neural_forecast=forecast(cpi_neural, h=12)
 
@@ -194,13 +140,11 @@ autoplot(cpi_neural_forecast)+
   xlab("Year")+
   ylab("CPI Index")
 
-#accuracy(cpi_neural_forecast,cpi_test) # Accuracy of the forecast with the actual values
-
 ## Actual vs Forecast in Neural Network Model ##
 
-cpi_neural_forecas1 = as.numeric(t(as.data.frame(cpi_neural_forecast))[,1:2])
+cpi_neural_forecast1 = as.numeric(t(as.data.frame(cpi_neural_forecast))[,1:2])
 
-cpi_neural_forecast2 = na.omit(cpi_neural_forecas1)
+cpi_neural_forecast2 = na.omit(cpi_neural_forecast1)
 
 cpi_bind_neural=cbind(cpi_test,cpi_neural_forecast2)
 
@@ -225,10 +169,6 @@ cpi_ets <-ets(cpi_train)
 cpi_ets_forecast=forecast(cpi_ets, h=12) # h in forecast signifies the duration of period for which we are forecasting
 
 autoplot(cpi_ets_forecast, xlab="Time", ylab="CPI") + ggtitle("India | Forecast CPI for Cereals and products using ETS")
-
-#summary(cpi_ets) # Summary of the model
-
-#accuracy(cpi_ets_forecast,cpi_test) # Accuracy of the forecast with the actual values
 
 ## Actual vs Forecast in ETS Model ##
 
